66bfe9e96e391f8bd57237b4492b797110974e06
philgrimaldi <phillip.grimaldi@gmail.com>
2015-01-30 16:27:12 -0600

    Writes bars in order
    writes bars in order, added topic labels, div wrapper

--- question_embed.js
+++ question_embed.js
@@ -1,13 +1,25 @@
 Qualtrics.SurveyEngine.addOnload(function()
 {
 	/*Place Your Javascript Below This Line*/
-
-		var bar1 = "<div style='height:22px;border-radius:2px;width:100%;border:1px solid #4887F1'><div style='background-color:#4887F1;height:20px;border-radius:2px;width:70%'></div></div>";
-		var bar2 = "<div style='height:22px;border-radius:2px;width:100%;border:1px solid #4887F1'><div style='background-color:#4887F1;height:20px;border-radius:2px;width:40%'></div></div>";
-
+		var outerdiv = "<div style='height:22px;border-radius:2px;width:100%;border:1px solid #4887F1'>";
+		var bar1 = outerdiv + "<div style='background-color:#4887F1;height:20px;border-radius:2px;width:10%'></div></div>";
+		var bar2 = outerdiv + "<div style='background-color:#4887F1;height:20px;border-radius:2px;width:20%'></div></div>";
+		var bar3 = outerdiv + "<div style='background-color:#4887F1;height:20px;border-radius:2px;width:30%'></div></div>";
+		var bar4 = outerdiv + "<div style='background-color:#4887F1;height:20px;border-radius:2px;width:40%'></div></div>";
 		
-		var bars = [bar1, bar2]
+		var bars = [bar1, bar2, bar3, bar4];
+		var topicLabels = ["Topic A", "Topic B", "Topic C", "Topic D"];
+	
+		var analyticsReport = "";
+	
 		$j.each(bars, function( i,bar ) {
-  			$j("#Questions").after(bar + "<br>");
+			analyticsReport = analyticsReport + topicLabels[i] + bar + "<br>";
+  			//$j("#Questions").after(topics[i] + bar + "<br>"); //Vi--I constructed the div first before putting after, otherwise the order was reversed
 		});
+		
+		//wrap the analytics for formatting
+		analyticsReport = "<div id='analyticswrapper'>" + analyticsReport + "</div>";
+	
+		$j("#Questions").after(analyticsReport);
 });
+
